1cd966fe79ad980d19a57c4eaa47e57b8ca2fc2e
John <john@onejohn.org>
2017-11-21 14:20:23 -0500

    some updates

--- src/orbit/core.clj
+++ src/orbit/core.clj
@@ -1,30 +1,51 @@
 (ns orbit.core
   (:gen-class))
 
+(defn numerator [n] (if (= (type n) clojure.lang.Ratio) (clojure.core/numerator n) n))
+(defn denominator [n] (if (= (type n) clojure.lang.Ratio) (clojure.core/denominator n) 1))
+
 (defn sqrt-n [S]
   (letfn [(abs [n] (if (neg? n) (- n) n))
     (sqrt-r [x]
-      (if (> x (abs (- S (*' x x))))
+      (if (>= x (abs (- S (*' x x))))
         x
-        (recur (bigint (/ (+ x (/ S x)) 2)))))]
-    (sqrt-r (/ S 2))))
+        (let [v (/ (+ x (/ S x)) 2)
+              n (numerator v)
+              d (denominator v)
+              vr (+ (quot n d) (if (< (/ d 2) (rem n d)) 1 0))]
+        (recur vr))))]
+    (sqrt-r 2)))
 
 (def G 1N)
 
-(defn compute-acceleration [p1 p2]
+(defn planet-acceleration [p1 p2]
   (if (= p1 p2) p1
     (let [dx (- (:x p2) (:x p1))
           dy (- (:y p2) (:y p1))
-          d-sqr (+ (*' dx dx) (*' dy dy))
+          dz (- (:z p2) (:z p1))
+          d-sqr (+ (*' dx dx) (*' dy dy) (*' dz dz))
           a (/ (* G (:mass p2)) d-sqr)
           d (sqrt-n d-sqr)
           ax (* a (/ dx d))
           ay (* a (/ dy d))
+          az (* a (/ dz d))
           ]
-      {:ax ax :ay ay})))
+      {:ax ax :ay ay :az az})))
 
-(defn gravity-step [dt planets]
- (map (fn [p] (apply merge-with + (map #(compute-acceleration p %) planets))) planets))
+(defn compute-acceleration [planets]
+  (map (fn [p] (apply merge-with + (map #(planet-acceleration p %) planets))) planets))
+
+(defn compute-velocity [dt planets]
+  (map (fn [p] (assoc p
+                      :vx (+ (:vx p) (* (:ax p) dt))
+                      :vy (+ (:vy p) (* (:ay p) dt))
+                      :vz (+ (:vz p) (* (:az p) dt)))) planets))
+
+(defn update-location [dt planets]
+  (map (fn [p] (assoc p
+                      :x (+ (:x p) (* (:vx p) dt))
+                      :y (+ (:y p) (* (:vy p) dt))
+                      :z (+ (:z p) (* (:vz p) dt)))) planets))
 
 (defn -main
   "I don't do a whole lot ... yet."
